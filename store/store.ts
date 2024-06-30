77c712dc1d68ae584a163119d58675485622d71a
unknown <tomfraczekdev@gmail.com>
2024-06-30 20:50:50 +0200

    feat: add redux persist

--- store/store.ts
+++ store/store.ts
@@ -1,13 +1,30 @@
 import { configureStore } from "@reduxjs/toolkit";
 import taskReducer from "./taskSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage,
+};
+
+const persistedReducer = persistReducer(persistConfig, taskReducer);
 
 const store = configureStore({
   reducer: {
-    tasks: taskReducer,
+    tasks: persistedReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST"],
+        ignoredPaths: ["register"],
+      },
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
+export const persistor = persistStore(store);
 export default store;
